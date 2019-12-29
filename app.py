c29791c2c67b5a504ec1c1dd15f7c5961071b46f
pennykkent <58962700+pennykkent@users.noreply.github.com>
2019-12-29 19:05:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -104,26 +104,22 @@ def loadPMJson():
         for ele in data:
             pm_site[ele['SiteName']] = ele['PM2.5']
 
-def getCls(cls_prefix):
-    ret_cls = []
-    urlstr = 'https://course.thu.edu.tw/search-result/10/1/'
-    postfix = '/all/all'
-    
-    qry_cls = urlstr + cls_prefix + postfix
-    
-    resp = requests.get(qry_cls)
-    resp.encoding = 'utf-8'
-    soup = BeautifulSoup(resp.text, 'lxml')
-    clsrooms = soup.select('table.aqua_table tbody tr')
-    for cls in clsrooms:
-        cls_info = cls.find_all('td')[1]
-        cls_name = cls_info.text.strip()
-        sub_url = 'https://course.thu.edu.tw' + cls_info.find('a')['href']
-        ret_cls.append(cls_name + " " + sub_url)
-        break
-#         ret_cls = ret_cls + sub_url + "\n"
-
-    return ret_cls
+def eyny_movie():
+    target_url = 'http://www.eyny.com/forum-205-1.html'
+    print('Start parsing eynyMovie....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ''
+    for titleURL in soup.select('.bm_c tbody .xst'):
+        if pattern_mega(titleURL.text):
+            title = titleURL.text
+            if '11379780-1-3' in titleURL['href']:
+                continue
+            link = 'http://www.eyny.com/' + titleURL['href']
+            data = '{}\n{}\n\n'.format(title, link)
+            content += data
+    return content
         
             
 import os
